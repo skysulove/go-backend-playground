8bee12dc6c82e250dd07518f1743fea7d63ac8d5
Lex Gnatovsky <skysurfersky@gmail.com>
2026-01-05 18:56:12 +0300

    day 2: added CLI commands with functions

--- main.go
+++ main.go
@@ -1,7 +1,52 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Start. Day 1.")
+	args := os.Args[1:] // все оргументы кроме имени программы
+	
+	if len(args) == 0 {
+		printHelp()
+		return
+	}
+	
+	command := args[0]
+	
+	switch command {
+	case "ping":
+		ping(args[1:])
+	case "hello":
+		sayHello(args[1:])
+	default:
+		fmt.Println("Unknown command:", command)
+		printHelp()
+	}
+}
+
+//Выводим помощь
+func printHelp() {
+	fmt.Println("Usage:")
+	fmt.Println(" go run main.go ping <something>")
+	fmt.Println(" go run main.go hello <name>")
+}
+
+//Простуйшая функция ping
+func ping(args []string){
+	if len(args) == 0 {
+		fmt.Println("ping what?")
+		return
+	}
+	fmt.Println("Pong to", args[0])
+}
+
+//Функция hello
+func sayHello(args []string) {
+	if len(args) == 0 {
+		fmt.Println("Hello, stranger!")
+		return
+	}
+	fmt.Println("Hello,", args[0]+"!")
 }
